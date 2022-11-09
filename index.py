a36bf1fd6ef955a2b63c1a1b84906581a5ca21d2
Lotta Tanner <lotta.tanner@helsinki.fi>
2022-11-09 22:17:39 +0200

    bugikorjaus

--- index.py
+++ index.py
@@ -11,4 +11,7 @@ y = int(input("luku 2: "))
 print(f"{summa(x, y)}")
 print(f"{erotus(x, y)}")
 
-logger("lopetetaan")
+logger("lopetetaan ohjelma")
+
+print("goodbye!") # lisäys bugikorjaus-branch
+
