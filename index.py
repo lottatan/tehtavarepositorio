b568787f71a4b9a0ba5faad82ecfaea93e63d90c
Lotta Tanner <lotta.tanner@helsinki.fi>
2022-11-10 13:27:08 +0200

    index.py

--- index.py
+++ index.py
@@ -4,14 +4,13 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan") # muutos masterissa
+logger("aloitetaan ohjelma") 
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"{summa(x, y)}") #muutos masterissa
+print(f"{erotus(x, y)}") #muutos masterissa
 
 logger("lopetetaan ohjelma")
-
-print("goodbye!") # lisäys bugikorjaus-branch
+print("goodbye!")
 
